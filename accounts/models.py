ffa077bb3772dc8f65e16608b0973a57795528e5
Seth250 <ayowaleakintayo@gmail.com>
2020-06-01 23:01:50 +0100

    made changes to the accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,12 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
+	date_of_birth = models.DateField(blank=True, null=True)
+	profile_image = models.ImageField(default='default.png', upload_to='profile_pictures')
 
 	def __str__(self):
 		return self.username
