554907cd3c00bdbe0b55eb8b6b20bd77be0ddd5a
Seth250 <ayowaleakintayo@gmail.com>
2020-06-04 13:45:28 +0100

    added logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 from .views import UserCreateView
 
 app_name = 'accounts'
 
 urlpatterns = [
 	path('signup/', UserCreateView.as_view(), name='signup'),
-	path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-	path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout')
+	path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import UserSignUpForm
 from django.contrib.auth import get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.views.generic import (
 	CreateView)
@@ -8,10 +9,11 @@ from django.views.generic import (
 
 # Create your views here.
 
-class UserCreateView(CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
 	model = get_user_model()
 	template_name = 'accounts/signup.html'
 	form_class = UserSignUpForm
+	success_message = 'Your Account has been succesfully Created, Login to Continue!'
 
 	def get_success_url(self):
 		return reverse("accounts:login")
