c94dd0e9982988e2d934d656645dee444e2c84ae
Seth250 <ayowaleakintayo@gmail.com>
2020-06-06 21:33:34 +0100

    more styling

--- accounts/static/accounts/js/main.js
+++ accounts/static/accounts/js/main.js
@@ -1,33 +1,27 @@
 
-function togglePasswordVisibility(elementId){
-	const elementName = JSON.stringify(elementId.split("-").slice(-1))
-	console.log(elementName)
-	// let passwordElement = document.getElementById(`id_${}`);
-	// let toggleIcon = document.getElementById(`toggler-icon-{id}`);
-	// // console.log(passwordElement, toggleIcon);
-	// if (passwordElement.type === 'password'){
-	// 	toggleIcon.className = 'far fa-eye';
-	// 	passwordElement.type = 'text';
-	// }else {
-	// 	toggleIcon.className = 'far fa-eye-slash';
-	// 	passwordElement.type = 'password';
-	// }
+function togglePasswordVisibility(togglerId){
+	const [, passwordElementName] = togglerId.split("-");
+	const passwordElement = document.getElementById(`id_${passwordElementName}`);
+	const toggleIcon = document.getElementById(`toggler-icon-${passwordElementName}`);
+	
+	if (passwordElement.type === 'password'){
+		toggleIcon.className = 'far fa-eye';
+		passwordElement.type = 'text';
+	}else {
+		toggleIcon.className = 'far fa-eye-slash';
+		passwordElement.type = 'password';
+	}
 }
 
-function initialize(){
-	let togglerElements = document.getElementsByClassName("password-toggler");
-	// console.log(togglerElements);
+const initialize = () => {
+	const togglerElements = document.getElementsByClassName("password-toggler");
 
 	for (let i=0; i < togglerElements.length; i++){
-		togglerElements[i].addEventListener('click', () => {
-			togglePasswordVisibility(togglerElements[i].id)
+		togglerElements[i].addEventListener('click', function(){
+			togglePasswordVisibility(this.id);
 		}, false)
 	}
-	// togglerElements.forEach((element) => {
-	// 	element.addEventListener('click', function(){
-	// 		togglePasswordVisibility(element.id)
-	// 	}, false)
-	// })
 }
+
 
 window.addEventListener('load', initialize, false);
