5806fb1e41a91c446878c7b8f38179292b601fd5
Seth250 <ayowaleakintayo@gmail.com>
2020-10-22 13:50:31 +0100

    updated styling

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,18 @@
 
+function toggleNavBar(){
+    const navItems = document.querySelector('.nav-bar__items');
+    navItems.classList.toggle('responsive-menu');
+}
+
 function initialize(){
     const messageBox = document.querySelector('.message-container');
     if (messageBox){
         const close = document.querySelector('.close');
         close.addEventListener('click', () => messageBox.classList.add('hide'), false);
     }
+
+    const hamburgerMenu = document.querySelector('.nav-bar__hamburger');
+    hamburgerMenu.addEventListener('click', toggleNavBar, false);
 }
 
 window.addEventListener('load', initialize, false);
